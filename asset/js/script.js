f22553cbc6ad70c2f00f2807bb91a807b835755e
speducation <123660808+speducation@users.noreply.github.com>
2023-01-27 01:26:52 +0630

    Update script.js
    Signed-off-by: speducation <123660808+speducation@users.noreply.github.com>

--- asset/js/script.js
+++ asset/js/script.js
@@ -16,16 +16,16 @@ class EngClass
             },
             {
                 name : "Wa Thone Aung",
-                img : "asset/img/profile.jpg",
+                img : "asset/img/speducation.jpg",
                 nickname : "Wa Thone",
                 phone : "09696699636",
                 facebook : "Wa Thone Aung",
                 fburl : "https://facebook.com/wathone.aung.3597"
             },
             {
-                name: "Wutt Hmone Htun",
-                img : "asset/img/WuttHmone.png",
-                nickname : "WM",
+                name: "Wutt Hmone Thin",
+                img : "asset/img/wutthmone.jpg",
+                nickname : "WMT",
                 phone : "09440263036",
                 facebook : "Wutt Hmone",
                 fburl : "https://facebook.com/profile.php?id=100087767780888"
@@ -40,7 +40,7 @@ class EngClass
             },
             {
                 name: "Thet Wai Soe",
-                img : "asset/img/ThetWaiSoe.png",
+                img : "asset/img/speducation.jpg",
                 nickname : "Thet Wai Soe",
                 phone : "09671775854",
                 facebook : "Thet Wai Soe",
@@ -56,11 +56,67 @@ class EngClass
             },
             {
                 name : "Freddy Minn",
-                img : "asset/img/freddyminn.jpg",
+                img : "asset/img/freddy.jpg",
                 nickname : "Freddy",
                 phone : "09266957979",
                 facebook : "Phyo Wai Min",
                 fburl : "https://facebook.com/mgphyowaimin"
+            },
+            {
+                name : "Niky Win Chester",
+                img : "asset/img/niky.jpg",
+                nickname : "Niky",
+                phone : "09799448742",
+                facebook : "Niky Win Chester",
+                fburl : "https://facebook.com/profile.php?id=100072015561421"
+            },
+            {
+                name : "Thet Hmue Chit",
+                img : "asset/img/thethmuechit.jpg",
+                nickname : "TMC",
+                phone : "09451059029",
+                facebook : "SP Education",
+                fburl : "https://facebook.com/SP-Education-868084966887176"
+            },
+            {
+                name : "Thet Jue Chit",
+                img : "asset/img/thethjuechit.jpg",
+                nickname : "TJC",
+                phone : "09451059029",
+                facebook : "SP Education",
+                fburl : "https://facebook.com/SP-Education-868084966887176"
+            },
+            {
+                name : "Thin Thin Aung",
+                img : "asset/img/thinthinaung.jpg",
+                nickname : "TTA",
+                phone : "09979094272",
+                facebook : "SP Education",
+                fburl : "https://facebook.com/SP-Education-868084966887176"
+            },
+            {
+                name : "Hein Htet",
+                img : "asset/img/speducation.jpg",
+                nickname : "HH",
+                phone : "097777526840",
+                facebook : "Hein Htet",
+                fburl : "https://facebook.com/profile.php?id=100087741926749"
+            },
+            {
+                name : "Wai Yamon Phoo",
+                img : "asset/img/waiyamonphoo.jpg",
+                nickname : "WYMP",
+                phone : "09421122206",
+                facebook : "Wai Yamon Phoo",
+                fburl : "https://facebook.com/profile.php?id=100075359192108"
+            },
+            {
+                name : "May Kine Oo",
+                img : "asset/img/waiyamonphoo.jpg",
+                nickname : "hh",
+                phone : "09421122206",
+                facebook : "SP Education",
+                fburl : "https://facebook.com/SP-Education-868084966887176"
             }
         ]
 
